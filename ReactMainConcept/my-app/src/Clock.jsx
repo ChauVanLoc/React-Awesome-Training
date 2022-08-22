c9c58294b336b074e799b26d2e9d13e7046aba66
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-08-22 18:40:12 +0700

    State và lifecycle trong class component

---
+++ ReactMainConcept/my-app/src/Clock.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: {
+        created: new Date().toLocaleTimeString()
+      },
+      seconds: {
+        created: new Date().getSeconds()
+      }
+    }
+    this.date = '22/8/2022'
+  }
+
+  getTime = () => {
+    // previousState.time !== newState.time
+    // previousState.time.created !== newState.time.created
+
+    const newState = {
+      ...this.state,
+      time: {
+        created: new Date().toLocaleTimeString()
+      },
+      seconds: { created: new Date().getSeconds() }
+    }
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.time.created}</h2>
+        <h2>It is {this.state.seconds.created}</h2>
+
+        <h3>Is is {this.date}</h3>
+        <button onClick={this.getTime}>Get Time</button>
+      </div>
+    )
+  }
+}
--- ReactMainConcept/my-app/src/index.js
+++ ReactMainConcept/my-app/src/index.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import Welcome from './Welcome'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-//<React.StrictMode>
-//<App />
-//</React.StrictMode>
-
-const element = (
-  <div>
-    <Welcome name='Sara' age={26} />
-    <Welcome name='Helen' age={27} />
-    <Welcome name='Alex' age={28} />
-  </div>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
-root.render(element)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
