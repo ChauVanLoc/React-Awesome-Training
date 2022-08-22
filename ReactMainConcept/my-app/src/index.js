981022a1b8c93eec35383947e9dc5823bf9c61a3
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-08-22 17:46:20 +0700

    Component và props

--- ReactMainConcept/my-app/src/index.js
+++ ReactMainConcept/my-app/src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
-// const element = <h1>Hello world!</h1>
+import Welcome from './Welcome'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-  root.render(element)
-}
 
-setInterval(tick, 1000)
+//<React.StrictMode>
+//<App />
+//</React.StrictMode>
+
+const element = (
+  <div>
+    <Welcome name='Sara' age={26} />
+    <Welcome name='Helen' age={27} />
+    <Welcome name='Alex' age={28} />
+  </div>
+)
+root.render(element)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
