3b06f1ad54b7018a64bef38785a3c67d713a3f76
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-08-23 11:06:18 +0700

    Cách dùng Props và PropTypes

--- ReactMainConcept/my-app/src/App.js
+++ ReactMainConcept/my-app/src/App.js
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import BareInput from './BareInput'
 import Clock from './Clock'
+import Layout from './Layout'
 
 function App() {
-  const [name, setName] = useState('Casio')
-  const [visible, setVisible] = useState(true)
+  // const [name, setName] = useState('Casio')
+  // const [visible, setVisible] = useState(true)
   return (
     <div className='App'>
-      <button onClick={() => setName('Apple')}>Change name</button>
+      {/* <button onClick={() => setName('Apple')}>Change name</button>
       <button onClick={() => setVisible(false)}>Hide Clock component</button>
-      {visible && <Clock name={name} />}
+      {visible && <Clock name={name} />} */}
+      <Layout>
+        <h1>Hello</h1>
+        <BareInput
+          type='password'
+          value='100'
+          autoFocus
+          className='input-control'
+          onChange={() => {}}
+        />
+      </Layout>
     </div>
   )
 }
