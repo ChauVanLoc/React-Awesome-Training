0cbcc34f73138841ad656d26ef1807d208454464
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-09-07 11:10:56 +0700

    Nguyên tắc khi dùng hooks

--- ReactHook/my-app/src/App.js
+++ ReactHook/my-app/src/App.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import RuleOfHook from './ruleOfHook'
 import User from './User'
 
 function App() {
   const [isShow, setIsShow] = useState(true)
   return (
     <div className='App'>
-      {isShow && <User />}
+      {/* {isShow && <User />}
       <button onClick={() => setIsShow((prevState) => !prevState)}>
         Change isShow
-      </button>
+      </button> */}
+      <RuleOfHook />
     </div>
   )
 }
