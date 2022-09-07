0436be6363b0a72ddd8fdecaeb9cfc7b51885624
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-09-07 15:20:14 +0700

    Automatic Batching

--- ReactHook/my-app/src/App.js
+++ ReactHook/my-app/src/App.js
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import RuleOfHook from './ruleOfHook'
-import User from './User'
+import AutoBatching from './AutoBatching'
+// import RuleOfHook from './ruleOfHook'
+// import User from './User'
 
 function App() {
-  const [isShow, setIsShow] = useState(true)
+  // const [isShow, setIsShow] = useState(true)
   return (
     <div className='App'>
       {/* {isShow && <User />}
       <button onClick={() => setIsShow((prevState) => !prevState)}>
         Change isShow
       </button> */}
-      <RuleOfHook />
+      {/* <RuleOfHook /> */}
+      <AutoBatching />
     </div>
   )
 }
