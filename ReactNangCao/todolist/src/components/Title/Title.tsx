d20ccfe894dd307a5d55d2ef286eda5631f216ec
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-09-22 17:43:53 +0700

    React.memo

---
+++ ReactNangCao/todolist/src/components/Title/Title.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './title.module.scss'
+
+type TitleProps = {
+  address: {
+    street: string
+  }
+}
+
+function Title(props: TitleProps) {
+  console.log(props.address)
+  return <h1 className={styles.title}>To do list typescript</h1>
+}
+
+function equal(prevProp: TitleProps, nextProps: TitleProps) {
+  return prevProp.address.street === nextProps.address.street
+}
+
+export default React.memo(Title, equal)
