24e02993a460deb906a8646199439be770364008
Du Thanh Duoc <duthanhduoc@gmail.com>
2022-09-27 19:36:41 +0700

    startTransition và useTransition

--- ReactNangCao/todolist/src/components/ProductList/ProductList.tsx
+++ ReactNangCao/todolist/src/components/ProductList/ProductList.tsx
@@ -1,4 +1,4 @@
-import { useDeferredValue, useEffect, useState } from 'react'
+import React, { useDeferredValue, useTransition, useEffect, useState } from 'react'
 
 const ProductCards = ({ name }: { name: string }) => {
   const [products, setProducts] = useState<string[]>([])
@@ -26,18 +26,24 @@ const ProductCards = ({ name }: { name: string }) => {
 
 export default function ProductList() {
   const [name, setName] = useState<string>('')
-  const deferredName = useDeferredValue(name)
+  const [deferredName, setDeferredName] = useState<string>('')
+  const [pending, startTransition] = useTransition()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setName(value)
+    startTransition(() => {
+      setDeferredName(value)
+    })
+  }
+  // console.log('pending', pending)
+  // const deferredName = useDeferredValue(name)
   return (
     <div>
       <h1>Product List</h1>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type='text'
-        placeholder='type to render'
-        autoFocus
-      />
-      <ProductCards name={deferredName} />
+      <input value={name} onChange={handleChange} type='text' placeholder='type to render' autoFocus />
+      {pending && <div>Loading...</div>}
+      {!pending && <ProductCards name={deferredName} />}
     </div>
   )
 }
